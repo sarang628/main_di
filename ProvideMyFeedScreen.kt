8d4c298edff656dee71ee5d2f9f07b10e1dd6568
sryang <sarang628@gmail.com>
2024-05-03 15:09:17 +0800

    add MainMyFeedScreen

---
+++ ProvideMyFeedScreen.kt
@@ -0,0 +1,31 @@
+package com.sarang.torang.di.main_di
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavHostController
+import com.sarang.torang.compose.MainMyFeedScreen
+import com.sarang.torang.di.bottomsheet.provideFeedMenuBottomSheetDialog
+import com.sarang.torang.di.comment_di.provideCommentBottomDialogSheet
+import com.sarang.torang.di.feed_di.provideMyFeedScreen
+import com.sarang.torang.di.report_di.provideReportModal
+import com.sryang.torangbottomsheet.di.bottomsheet.provideShareBottomSheetDialog
+
+@Composable
+fun ProvideMyFeedScreen(
+    navController: NavHostController,
+    reviewId : Int
+) {
+    var show by remember { mutableStateOf(false) }
+
+    MainMyFeedScreen(
+        myFeedScreen = provideMyFeedScreen(navController = navController, reviewId = reviewId, onShowComment = {show = true }),
+        commentBottomSheet = provideCommentBottomDialogSheet(show) { show = false },
+        menuDialog = provideFeedMenuBottomSheetDialog(),
+        shareDialog = provideShareBottomSheetDialog(),
+        reportDialog = provideReportModal(),
+        onEdit = { navController.navigate("modReview/${it}") }
+    )
+}
