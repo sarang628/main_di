b3e832f7a360c2d0a98caf718eba8deaab944c9f
sryang <sarang628@gmail.com>
2024-08-19 22:19:47 +0800

    update

--- ProvideMainDialog.kt
+++ ProvideMainDialog.kt
@@ -8,7 +8,6 @@ import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.MainDialogs
 import com.sarang.torang.di.bottomsheet.provideFeedMenuBottomSheetDialog
 import com.sarang.torang.di.report_di.provideReportModal
-import com.sarang.torang.di.torang.provideCommentBottomDialogSheet
 import com.sarang.torang.viewmodels.FeedDialogsViewModel
 import com.sryang.torangbottomsheet.di.bottomsheet.provideShareBottomSheetDialog
 
@@ -21,7 +20,7 @@ fun ProvideMainDialog(
     val uiState by dialogsViewModel.uiState.collectAsState()
     MainDialogs(
         uiState = uiState,
-        commentBottomSheet = { provideCommentBottomDialogSheet(rootNavController).invoke(it) { dialogsViewModel.closeComment() } },
+        commentBottomSheet = { /*provideCommentBottomDialogSheet(rootNavController).invoke(it) { dialogsViewModel.closeComment() }*/ },
         menuDialog = provideFeedMenuBottomSheetDialog(),
         shareDialog = provideShareBottomSheetDialog(),
         reportDialog = provideReportModal(),
