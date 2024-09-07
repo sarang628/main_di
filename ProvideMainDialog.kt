1675aab2e26e20355895882ee5d81a194845b052
sryang <sarang628@gmail.com>
2024-09-07 12:48:30 +0800

    update

--- ProvideMainDialog.kt
+++ ProvideMainDialog.kt
@@ -11,6 +11,15 @@ import com.sarang.torang.di.report_di.provideReportModal
 import com.sarang.torang.viewmodels.FeedDialogsViewModel
 import com.sryang.torangbottomsheet.di.bottomsheet.provideShareBottomSheetDialog
 
+/**
+ * 메인 다이얼로그 제공
+ *
+ * 메인 화면에서 사용하는 다이얼로그
+ * 리뷰 신고, 리뷰 메뉴, 리뷰 공유, 리뷰 댓글 보기/작성 다이얼로그
+ * @param dialogsViewModel 다이얼로그 뷰모델
+ * @param rootNavController 루트 네비게이션 컨트롤러
+ * @param contents 화면
+ */
 @Composable
 fun ProvideMainDialog(
     dialogsViewModel: FeedDialogsViewModel = hiltViewModel(),
