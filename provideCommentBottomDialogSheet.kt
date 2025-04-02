df3fef39986412feb85e5dfbf505596d81ab8564
sryang <sarang628@gmail.com>
2025-04-02 14:09:00 +0900

    fix not showing comment dialog
    - add show state in provideCommentBottomzDialogSheet in main_di

--- provideCommentBottomDialogSheet.kt
+++ provideCommentBottomDialogSheet.kt
@@ -1,18 +1,36 @@
 package com.sarang.torang.di.main_di
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.sarang.torang.RootNavController
 import com.sarang.torang.di.comment_di.CommentBottomSheet
 import com.sarang.torang.di.image.provideTorangAsyncImage
+import kotlinx.coroutines.flow.Flow
 
 fun provideCommentBottomDialogSheet(rootNavController: RootNavController): @Composable (reviewId: Int?, onHidden: (() -> Unit)) -> Unit =
     { reviewId, onHidden ->
+        var currentReviewId: Int? by remember { mutableStateOf(null) }
+        var show by remember { mutableStateOf(false) }
+
+        if (currentReviewId != reviewId) {
+            currentReviewId = reviewId;
+            show = true
+        }
+
+        Log.d("__sryang", "show : ${show}")
+
         CommentBottomSheet(
             reviewId = reviewId,
             onDismissRequest = onHidden,
             onHidden = {
                 onHidden.invoke()
+                show = false
             },
+            show = show,
             content = { },
             image = provideTorangAsyncImage(),
             onImage = {
