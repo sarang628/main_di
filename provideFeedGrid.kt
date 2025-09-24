2364aae76cdbf20f9493668b567d686762acd239
sryang <sarang628@gmail.com>
2025-09-24 15:06:10 +0800

    fix wrong word

--- provideFeedGrid.kt
+++ provideFeedGrid.kt
@@ -3,6 +3,6 @@ package com.sarang.torang.di.main_di
 import androidx.compose.runtime.Composable
 import com.sarang.torang.di.feedgrid_di.ProvideTorangGrid
 
-fun provideFeedGreed(): @Composable () -> Unit = {
+fun provideFeedGrid(): @Composable () -> Unit = {
     ProvideTorangGrid()
 }
