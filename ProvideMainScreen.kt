223353ee6e114842344eec7703abe117c5bec7ab
sryang <sarang628@gmail.com>
2025-10-05 09:27:46 +0900

    comment error

--- ProvideMainScreen.kt
+++ ProvideMainScreen.kt
@@ -8,24 +8,21 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.MainScreen
-import com.sarang.torang.compose.MainScreenState
 import com.sarang.torang.compose.feed.internal.components.LocalFeedImageLoader
 import com.sarang.torang.compose.feed.state.FeedScreenState
 import com.sarang.torang.compose.feed.state.rememberFeedScreenState
 import com.sarang.torang.compose.feed.type.LocalBottomDetectingLazyColumnType
 import com.sarang.torang.compose.feed.type.LocalPullToRefreshLayoutType
-import com.sarang.torang.compose.rememberMainScreenState
 import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
 import com.sarang.torang.di.chat_di.ChatActivity
 import com.sarang.torang.di.feed_di.CustomBottomDetectingLazyColumnType
 import com.sarang.torang.di.feed_di.customPullToRefresh
-import com.sarang.torang.di.image.provideImageLoader
 import com.sarang.torang.di.pinchzoom.PinchZoomImageBox
 import com.sarang.torang.viewmodels.FeedDialogsViewModel
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 fun provideMainScreen(rootNavController: RootNavController,
@@ -38,7 +35,6 @@ fun provideMainScreen(rootNavController: RootNavController,
 ) : @Composable () ->Unit = {
     val dialogsViewModel: FeedDialogsViewModel  = hiltViewModel()
     val feedScreenState : FeedScreenState       = rememberFeedScreenState()
-    val mainScreenState : MainScreenState       = rememberMainScreenState()
     val coroutineScope  : CoroutineScope        = rememberCoroutineScope()
     val context         : Context               = LocalContext.current
 
@@ -49,8 +45,6 @@ fun provideMainScreen(rootNavController: RootNavController,
     ) {
         PinchZoomImageBox {
             MainScreen(
-                swipeAblePager      = mainScreenState.isSwipeEnabled /*&& !zoomState.isZooming*/,
-                state               = mainScreenState,
                 feedGrid            = feedGrid,
                 myProfileScreen     = myProfileScreen,
                 findingMapScreen    = findingMapScreen,
@@ -63,30 +57,16 @@ fun provideMainScreen(rootNavController: RootNavController,
                         LocalBottomDetectingLazyColumnType provides CustomBottomDetectingLazyColumnType) {
                         FeedScreenWithProfile(
                             rootNavController   = rootNavController,
-                            feedNavController   = mainScreenState.feedNavController,
+                            feedNavController   = rememberNavController(),
                             dialogsViewModel    = dialogsViewModel,
                             feedScreenState     = feedScreenState,
                             onChat              = onChat,
-                            onAlarm             = { mainScreenState.goAlarm() },
-                            onMessage           = { ChatActivity.go(context, it) },
-                            scrollEnabled       = true/*!zoomState.isZooming*/,
-                            pageScrollable      = true/*!zoomState.isZooming*/,
-                            onPage = { page, isFirst, isLast ->
-                                /*if (isFirst || isLast) mainScreenState.swipeDisableForMillies(coroutineScope = coroutineScope)*/
-                            }
+                            onMessage           = { ChatActivity.go(context, it) }
                         )
                     }
                 },
-                onBottomMenu = {
-                    if (mainScreenState.isFeedPage && mainScreenState.latestDestination == "feed" && it == "feed") {
-                        mainScreenState.popToFeed()
-                    } else if (mainScreenState.latestDestination == "feed" && it == "feed") {
-                        coroutineScope.launch {
-                            feedScreenState.onTop() // 피드 화면 에서 피드 버튼을 눌렀을 때 리스트 최상단 이동
-                        }
-                    }
-                    mainScreenState.latestDestination = it
-                }
+                onBottomMenu = {},
+                consumeAlarm = {}
             )
         }
     }
