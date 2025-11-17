e4bf9f5a6953da0ea78d1f4abcdb669eec71ea2f
sryang <sarang628@gmail.com>
2025-11-17 15:34:58 +0900

    restore rootNavController in find module

--- ProvideFindScreenType.kt
+++ ProvideFindScreenType.kt
@@ -1,9 +1,16 @@
 package com.sarang.torang.di.main_di
 
+import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.type.FindScreenType
 import com.sarang.torang.di.finding_di.FindState
 import com.sarang.torang.di.finding_di.findingWithPermission
 
-fun provideFindScreenType(findState: FindState): FindScreenType = {
-    findingWithPermission(findState = findState).invoke()
+fun provideFindScreenType(
+    findState: FindState,
+    rootNavController: RootNavController = RootNavController()
+): FindScreenType = {
+    findingWithPermission(
+        findState = findState,
+        navController = rootNavController
+    ).invoke()
 }
