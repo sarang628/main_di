def71ae211e1a6a9e37a246a2ce9beacdb8221c9
sryang <sarang628@gmail.com>
2025-11-17 12:43:09 +0900

    refactor: move Compose parameters to CompositionLocal
    - reduces deeply nested parameter passing
    - improves readability and maintainability

--- ProvideMainDialog.kt
+++ ProvideMainDialog.kt
@@ -2,11 +2,17 @@ package com.sarang.torang.di.main_di
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.MainDialogs
+import com.sarang.torang.compose.type.LocalCommentBottomSheet
+import com.sarang.torang.compose.type.LocalMenuBottomSheet
+import com.sarang.torang.compose.type.LocalReportBottomSheetType
+import com.sarang.torang.compose.type.LocalRestaurantBottomSheet
+import com.sarang.torang.compose.type.LocalShareBottomSheet
 import com.sarang.torang.di.bottomsheet_di.provideFeedMenuBottomSheetDialog
 import com.sarang.torang.di.report_di.provideReportModal
 import com.sarang.torang.di.report_di.provideShareBottomSheetDialog
@@ -21,9 +27,8 @@ fun ProvideMainDialog(
 ) {
     val tag = "__ProvideMainDialog"
     val uiState by dialogsViewModel.uiState.collectAsState()
-    MainDialogs(
-        uiState = uiState,
-        commentBottomSheet = { reviewId ->
+    CompositionLocalProvider(
+        LocalCommentBottomSheet provides { reviewId ->
             provideCommentBottomDialogSheet(rootNavController).invoke(
                 CommentBottomDialogSheetData(
                     reviewId = reviewId,
@@ -32,10 +37,14 @@ fun ProvideMainDialog(
                 )
             )
         },
-        menuBottomSheet = provideFeedMenuBottomSheetDialog(),
-        shareBottomSheet = provideShareBottomSheetDialog(uiState.showShare),
-        reportBottomSheet = provideReportModal(),
-        restaurantBottomSheet = restaurantBottomSheet,
-        onEdit = rootNavController.modReview(),
-    )
+        LocalMenuBottomSheet provides provideFeedMenuBottomSheetDialog(),
+        LocalShareBottomSheet provides provideShareBottomSheetDialog(uiState.showShare),
+        LocalRestaurantBottomSheet provides restaurantBottomSheet,
+        LocalReportBottomSheetType provides provideReportModal()
+    ) {
+        MainDialogs(
+            uiState = uiState,
+            onEdit = rootNavController.modReview(),
+        )
+    }
 }
