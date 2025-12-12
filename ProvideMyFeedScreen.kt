50eba8a61fc31950ecae3d074536a0a565f5f4f8
sryang <sarang628@gmail.com>
2025-12-12 14:28:34 +0900

    feat: add navigation to detail my feed from profile screen

--- ProvideMyFeedScreen.kt
+++ ProvideMyFeedScreen.kt
@@ -2,14 +2,18 @@ package com.sarang.torang.di.main_di
 
 import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
-import com.sarang.torang.dialogsbox.compose.DialogsBoxViewModel
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.feed.FeedScreenByReviewId
-import com.sarang.torang.di.comment_di.CommentBottomDialogSheetData
+import com.sarang.torang.compose.feed.internal.components.LocalFeedImageLoader
+import com.sarang.torang.compose.feed.type.LocalFeedCompose
+import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
 import com.sarang.torang.di.dialogsbox_di.ProvideDialogsBox
+import com.sarang.torang.di.feed_di.CustomFeedCompose
+import com.sarang.torang.dialogsbox.compose.DialogsBoxViewModel
 import com.sryang.library.pullrefresh.rememberPullToRefreshState
 
 @Composable
@@ -31,8 +35,12 @@ fun ProvideMyFeedScreen(
         dialogsViewModel = dialogsViewModel,
         rootNavController = rootNavController,
     ) {
-        FeedScreenByReviewId(
-            reviewId = reviewId,
-        )
+        CompositionLocalProvider(LocalFeedImageLoader provides CustomFeedImageLoader(),
+            LocalFeedCompose provides CustomFeedCompose) {
+            FeedScreenByReviewId(
+                reviewId = reviewId,
+                onBack = { rootNavController.navController?.popBackStack() }
+            )
+        }
     }
 }
